31bd9bd382237949766bf2307145161305d6fd8f
jesus.rodriguez937 <jesus.rodriguez937@mppre.gob.ve>
2014-04-25 11:57:55 -0430

    Primer Comit del proyecto

--- system/core/CodeIgniter.php
+++ system/core/CodeIgniter.php
@@ -1,5 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/**
+/***
  * CodeIgniter
  *
  * An open source application development framework for PHP 5.1.6 or newer
